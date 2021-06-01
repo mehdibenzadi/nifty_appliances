8272e6356056f1ee53641ffb81204d56fcce12ec
Mehdi Benzadi <mehdi.benzadi@gmail.com>
2021-06-01 20:48:18 +0200

    Finally, the routes seem to work all well

--- app/models/appliance.rb
+++ app/models/appliance.rb
@@ -1,3 +1,3 @@
 class Appliance < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :appliances, only: [:show, :update,] do
-    get :add, on: :collection 
+  resources :appliances, only: [:show, :update] do
+    collection do 
+      get :add
+      post :link_user
+    end
   end
 end
