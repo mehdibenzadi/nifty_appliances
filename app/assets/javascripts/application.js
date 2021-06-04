20b20893563eb862dc2ca3fab0e8c6df42ea8f44
Francois Coulloudon <fcoulloudon@gmail.com>
2021-06-04 09:03:08 +0200

    Second commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+// Global
+//= require bootstrap
+//= require_tree ./theme/js
+//= require_tree ./theme/plugins
+
+// Custom files to include in application.js
+//= require tiny-slider
+//= require smooth-scroll
