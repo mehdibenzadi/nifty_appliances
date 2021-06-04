24d331a4867c29f7287543682a3e770e5cc1cc45
Francois Coulloudon <fcoulloudon@gmail.com>
2021-06-04 22:18:27 +0200

    ApexCharts working

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -2,3 +2,5 @@
 //= require ./theme/js/custom/apps/chat/chat.js
 //= require ./theme/js/custom/modals/create-app.js
 //= require ./theme/js/custom/modals/upgrade-plan.js
+//= require ./theme/plugins/formvalidation/dist/js/FormValidation.min.js
+//= require ./theme/plugins/formvalidation/dist/js/plugins/Bootstrap.min.js
