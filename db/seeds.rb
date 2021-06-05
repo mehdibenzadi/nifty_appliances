895b6fc733766c891828ad230b5a0247bef1644a
mathis ducasse <ducasse.mathis@gmail.com>
2021-06-05 15:28:33 +0200

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating serial numbers...'
+SerialNumber.create!({
+  value: "1236589"
+})
+SerialNumber.create!({
+  value: "1590607"
+})
+SerialNumber.create!({
+  value: "4789304"
+})
+SerialNumber.create!({
+  value: "1075628"
+})
+SerialNumber.create!({
+   value: "9068376"
+})
