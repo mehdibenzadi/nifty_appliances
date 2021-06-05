4b54a747d967ad6911eea323054268cb641d9d2e
mathis ducasse <ducasse.mathis@gmail.com>
2021-06-05 18:02:00 +0200

    These are the serial numbers

--- Gemfile
+++ Gemfile
@@ -66,3 +66,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#Gemfile
+gem "sidekiq"
+gem "sidekiq-failures", "~> 1.0"
--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,21 @@ Rails.application.routes.draw do
     unauthenticated do
       root to: 'pages#home', as: :visitors_url
     end
-  end  
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :appliances, only: [:show, :update] do
-    collection do 
+    collection do
       get :add
       post :link_user
     end
   end
-  
+
   resources :events, only: [:create]
+
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
