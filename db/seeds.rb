fd6c0f6b6c81f7c6e65ae62867bb5362e16b12ff
Francois Coulloudon <fcoulloudon@gmail.com>
2021-06-10 20:02:31 +0200

    Partial seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,43 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Creating users...'
+user = User.create! :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :repairer => false
+user = User.create! :email => 'marylin@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :repairer => false
+user = User.create! :email => 'joey@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :repairer => true
+user = User.create! :email => 'lucy@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret', :repairer => true
+
+puts 'Creating appliances...'
+appliance = Appliance.create! :model => 'AAZZCC', :serial_number => '1236589', :user_id => '1'
+appliance = Appliance.create! :model => 'AAZZCC', :serial_number => '1236589', :user_id => '1'
+appliance = Appliance.create! :model => 'AAZZCC', :serial_number => '1236589', :user_id => '1'
+
 puts 'Creating serial numbers...'
-SerialNumber.create!({
-  value: "1236589"
-})
-SerialNumber.create!({
-  value: "1590607"
-})
-SerialNumber.create!({
-  value: "4789304"
-})
-SerialNumber.create!({
-  value: "1075628"
-})
-SerialNumber.create!({
-   value: "9068376"
-})
-SerialNumber.create!({
-   value: "9876543"
-})
-SerialNumber.create!({
-   value: "2846928"
-})
-SerialNumber.create!({
-   value: "1234567"
-})
-SerialNumber.create!({
-   value: "7654321"
-})
-SerialNumber.create!({
-   value: "1324354"
-})
-SerialNumber.create!({
-   value: "7564243"
-})
-SerialNumber.create!({
-   value: "0798645"
-})
-SerialNumber.create!({
-   value: "0687922"
-})
+SerialNumber.create!({ value: "1236589" })
+SerialNumber.create!({ value: "1590607" })
+SerialNumber.create!({ value: "4789304" })
+SerialNumber.create!({ value: "1075628" })
+SerialNumber.create!({ value: "9068376" })
+SerialNumber.create!({ value: "9876543" })
+SerialNumber.create!({ value: "2846928" })
+SerialNumber.create!({ value: "1234567" })
+SerialNumber.create!({ value: "7654321" })
+SerialNumber.create!({ value: "1324354" })
+SerialNumber.create!({ value: "7564243" })
+SerialNumber.create!({ value: "0798645" })
+SerialNumber.create!({ value: "0687922" })
