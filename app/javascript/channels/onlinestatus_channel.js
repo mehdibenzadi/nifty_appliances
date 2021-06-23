93edcbce882da99e725b7e2908d4dafe8de5b17e
Francois Coulloudon <fcoulloudon@gmail.com>
2021-06-23 06:48:53 +0200

    No change

--- app/javascript/channels/onlinestatus_channel.js
+++ app/javascript/channels/onlinestatus_channel.js
@@ -1,20 +1,5 @@
 import consumer from "./consumer";
 
-/* const initOnlineChannel = () => {
-  console.log("Hello Mathis");
-  const onlineStatusButtons = document.querySelectorAll(".badge.badge-light-danger, .badge.badge-light-success");
-  console.log(onlineStatusButtons)
-  console.log(typeof onlineStatusButtons)
-  for (let status of onlineStatusButtons) {
-    const id = status.dataset.onlineStatusId;
-    consumer.subscriptions.create({ channel: "OnlinestatusChannel", id: id }, {
-      received(data) {
-        console.log(data); // called when data is broadcast in the cable
-      },
-    });
-  }
-} */
-
 const initOnlineChannel = () => {
   // Selection en JS de tous les tag online / offline (voir _online_status_button.html.erb)
   let onlineStatusButtons = document.querySelectorAll(".status-button");
@@ -33,30 +18,5 @@ const initOnlineChannel = () => {
   });
 }
 
-
-const initChatroomCable = () => {
-  const messagesContainer = document.getElementById('messages');
-  if (messagesContainer) {
-    const id = messagesContainer.dataset.chatroomId;
-
-    consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
-      received(data) {
-        console.log(data); // called when data is broadcast in the cable
-      },
-    });
-  }
-}
-
-
-  /*onlineStatusButtons.forEach((element) => {
-    const id = element.dataset.onlineStatusId;
-    console.log(id);  
-    consumer.subscriptions.create({ channel: "OnlinestatusChannel", id: id }, {
-      received(data) {
-        console.log(data); // called when data is broadcast in the cable
-      },
-    });
-  }) */
-
-  export default initOnlineChannel;
+export default initOnlineChannel;
 
